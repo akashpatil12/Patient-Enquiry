f54c1f7f3033b0b928bc39d2f23ed299a23f8d64
AkashpatilOA <akashpatil1415@gmail.com>
2024-01-10 13:15:06 +0530

    Home Page creation

--- Patient-Enquiry/src/app/app-routing.module.ts
+++ Patient-Enquiry/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 const routes: Routes = [
+  { path:'',component:HomePageComponent },
   { path:'navbar', component:NavbarComponent },
   { path:'footer', component:FooterComponent },
 ];
--- Patient-Enquiry/src/app/app.module.ts
+++ Patient-Enquiry/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
