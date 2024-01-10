82b79bb1b6cd32d4be8f13df2ae84d1ee197ceaa
AkashpatilOA <akashpatil1415@gmail.com>
2024-01-10 10:13:43 +0530

    bootstrap configuration

--- Patient-Enquiry/src/app/app-routing.module.ts
+++ Patient-Enquiry/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'navbar', component:NavbarComponent },
+  { path:'footer', component:FooterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
